291ae85b08664eda0400dc26ffad5b1463ffde98
Paulus0612 <paulusmanurung385@gmail.com>
2024-11-13 10:36:09 +0700

    Routes.js

---
+++ Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function AppRoutes() {
+   return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+}
+export default App;
